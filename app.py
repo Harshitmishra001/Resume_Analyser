2bde74054dbe7082f4acafa0452cc609bc0a8622
Harshit_Shadow <hmharsh123@gmail.com>
2024-08-15 21:05:02 +0530

    updation

--- app.py
+++ app.py
@@ -26,32 +26,41 @@ def extract_text_from_pdf():
         for page_num in range(num_pages):
             page = pdf_reader.pages[page_num]
             text += page.extract_text()
-
     return text
 
 def extract_details(text):
+    DRDO_Department=["Advanced Systems Laboratory (ASL)","Aerial Delivery Research and Development Establishment (ADRDE)","Aeronautical Development Establishment (ADE)","Armament Research & Development Establishment (ARDE)",
+                    "Centre For Air Borne System (CABS)","Centre for Artificial Intelligence & Robotics (CAIR)","Centre for Fire, Explosive and Environment Safety (CFEES)","Combat Vehicles Research & Development Establishment (CVRDE)","Defence Bio-Engineering & Electro Medical Laboratory (DEBEL)",
+                    "Defence Electronics Application Laboratory (DEAL)","Defence Electronics Research Laboratory (DLRL)","Defence Food Research Laboratory (DFRL)","Defence Geoinformatics Research Establishment (DGRE)","Defence Institute of High Altitude Research (DIHAR)","Defence Institute of Physiology & Allied Sciences (DIPAS)",
+                    "Defence Institute of Psychological Research (DIPR)","Defence Laboratory Jodhpur (DLJ)","Defence Materials and Stores Research and Development Establishment (DMSRDE)","Defence Metallurgical Research Laboratory (DMRL)","Defence Research & Development Laboratory (DRDL)","Defence Research Development Establishment (DRDE)","Defence Research Laboratory (DRL)","Defence Scientific Information & Documentation Centre (DESIDOC)","Defense young Scientist Laboratory - Artificial Intelligence (DYSL-AI)","DRDO Young Scientist Laboratory (DYSL-AT)",
+                    "DRDO Young Scientist Laboratory (DYSL-CT)","DRDO Young Scientist Laboratory (DYSL-QT)","DRDO Young Scientist Laboratory (DYSL-SM)","Electronics & Radar Development Establishment (LRDE)","Gas Turbine Research Establishment (GTRE)","High Energy Materials Research Laboratory (HEMRL)","Institute for Systems Studies & Analyses (ISSA)","Institute of Nuclear Medicine & Allied Sciences (INMAS)",
+                    "Institute of Technology Management (ITM)","Instruments Research & Development Establishment (IRDE)","Integrated Test Range (ITR)","Naval Materials Research Laboratory (NMRL)","Naval Physical & Oceanographic Laboratory (NPOL)","Naval Science & Technological Laboratory (NSTL)","Proof & Experimental Establishment (PXE)","Research & Development Establishment (Engrs.)","Research Centre Imarat (RCI)","Scientific Analysis Group (SAG)","Solid State Physics Laboratory (SSPL)","Terminal Ballistics Research Laboratory (TBRL)","Vehicles Research and Development Establishment (VRDE)"]
     prompt = """
     You are a helpful AI assistant. Your task is to extract details from the provided resume text. Respond in the following format:
     ```json
     [
         "name", 
-        "related to which field", 
-        "birthplace", 
+        "Qualifications", 
+        "What field Reseach Papers are on", 
         "total_experience_years", 
-        "current_or_most_recent_job_title" 
+        "Relevant_Department" 
     ]
     ```
     Where:
     -  If the information cannot be found, use "NA".
     -  "total_experience_years" should be an integer representing the total years of experience. If it's not mentioned, use "NA".
-    
+    - "Relevant_Department" should be from the list {DRDO_Department} which are the various departments under DRDO, select the one best for the resume.If none suitable , use "NA"
     Resume Text:
     ```
     {text}
     ```
+
     """
     try:
-        response = model.generate_content([prompt.format(text=text)])
+        response = model.generate_content([prompt.format(text=text)],safety_settings={
+        HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.	BLOCK_NONE,
+        HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
+    })
         extracted_data = response.text
 
         try:
